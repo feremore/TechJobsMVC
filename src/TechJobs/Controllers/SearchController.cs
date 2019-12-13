48a339e6bfb374d79e1db83e89dc97598a4b8b97
Kyle Loewenstein <kloewenmore@outlook.com>
2019-12-13 15:27:40 -0600

    added search all and by column table print and list all table print

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -12,6 +12,27 @@ namespace TechJobs.Controllers
             ViewBag.title = "Search";
             return View();
         }
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+           
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.title = "All Jobs Sorted By: "+searchTerm;
+                ViewBag.jobs = jobs;
+                ViewBag.columns = ListController.columnChoices;
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<string,string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "All " + searchType + " Values";
+                ViewBag.columns = ListController.columnChoices;
+                
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+        }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
